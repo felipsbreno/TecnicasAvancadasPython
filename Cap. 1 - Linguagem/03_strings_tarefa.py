92df7643d9136f5f4ace167bc80124688618fd50
felipsbreno <brendo.filipe2050@gmail.com>
2023-02-23 22:49:24 -0300

    resolvendo exercicios da aula

--- Cap. 1 - Linguagem/03_strings_tarefa.py
+++ Cap. 1 - Linguagem/03_strings_tarefa.py
@@ -4,12 +4,19 @@
 def main():
     # definindo alguns valores iniciais
     b = bytes([0x41, 0x42, 0x43, 0x44])
+    print(b)
+
+    s = "Sou uma string"
+    print(s)
 
     # TODO: Tente juntar os dois.
+    # print(s + b)
 
     # TODO: Bytes e strings precisam ser apropriadamente encoded
+    print(s + b.decode('utf-8'))
 
     # TODO: Faça o encode da string como UTF-32
+    print(s.encode('utf-32') + b)
 
 
 if __name__ == "__main__":
--- Cap. 1 - Linguagem/04_templstr_tarefa.py
+++ Cap. 1 - Linguagem/04_templstr_tarefa.py
@@ -1,6 +1,7 @@
 # Demonstração das funções de string templates
 from string import Template
 
+
 def main():
     # Formatação tradicional usando o método format()
     frase = "Você está assistindo {0} com {1}".format(
@@ -8,11 +9,19 @@ def main():
     print(frase)
 
     # TODO: Crie um template com placeholders
-
-
+    templ = Template("Você está assistindo o ${curso} com a ${instrutora}")
     # TODO: Use o método substitute passando argumentos nomeados
-
+    frase_2 = templ.substitute(
+        curso="Python Avançado", instrutora="Anna Hommann")
+    print(frase_2)
     # TODO: Use  o método substitute com um dicionário
+    dados = {
+        "curso": "Python Avançado",
+        "instrutora": "Anna Hommann",
+    }
+
+    frase_3 = templ.substitute(dados)
+    print(frase_3)
 
 
 if __name__ == "__main__":
